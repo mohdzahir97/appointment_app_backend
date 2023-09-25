c15b950adf9239c93031f1f8b5fe7cf944824fd8
mohdzahir97 <mohd.zahir2597@gmail.com>
2023-09-25 16:39:45 +0530

    initial commit

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,14 +1,24 @@
 
 const sendToken = (user, statusCode, res) => {
-  
+
   const token = user.getJWTToken();
 
   const options = {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
+    secure: true,
+    sameSite: "none",
     httpOnly: true,
   };
+
+  // res
+  //   .cookie('access_token', token, {
+  //     expires: new Date(Date.now() + 604800000),
+  //     secure: env.ENVIRONMENT === 'LIVE',
+  //     sameSite: env.ENVIRONMENT === 'LIVE' ? 'none' : 'lax',
+  //     httpOnly: true,
+  //   })
 
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
