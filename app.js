cfd5e3372ee02fb7863b48a9c915c8bf7d97252b
mohdzahir97 <mohd.zahir2597@gmail.com>
2023-09-25 15:54:43 +0530

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const cors=require("cors")
+const cors = require("cors")
 
 const errorMiddleware = require("./middleware/error");
 const { userRouter } = require("./routes/userRoutes");
@@ -20,17 +20,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ['http://localhost:3000', 'https://appointment-ifvu.onrender.com'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 
-app.use(cors({
-  origin:"*",
-  credentials:true
-}))
+
+app.use("/api/v1", userRouter)
 
 
-app.use("/api/v1",userRouter)
-
-
-app.use("/api/v1",meetingRoutes)
+app.use("/api/v1", meetingRoutes)
 
 
 
