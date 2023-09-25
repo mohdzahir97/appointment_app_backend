bf276a9d54866a7e2f37f158473be137529442ba
mohdzahir97 <mohd.zahir2597@gmail.com>
2023-09-25 15:49:55 +0530

    initial commit

--- app.js
+++ app.js
@@ -19,20 +19,12 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use(cors())
 
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
 
-// app.use(cors({
-//   origin:["*"],
-//   credentials:true
-// }))
+app.use(cors({
+  origin:"*",
+  credentials:true
+}))
 
 
 app.use("/api/v1",userRouter)
